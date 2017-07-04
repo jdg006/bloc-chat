03f4728ad486bf61bc0e9dddefc5cf8d2e70f133
jdg006 <jdg006@lvc.edu>
2017-07-04 19:57:37 -0400

    checkpoint 3

---
+++ app/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,31 @@
+(function(){
+function ModalCtrl ($uibModal, $log, Room) {
+
+  this.animationsEnabled = true;
+
+  this.open = function () {
+
+    var modalInstance = $uibModal.open({
+      animation: this.animationsEnabled,
+      templateUrl: '/templates/modal.html',
+      controller: 'ModalInstanceCtrl',
+      controllerAs: '$ctrl',
+
+    });
+
+    modalInstance.result.then(function (name) {
+      var room={$value:name};
+      Room.add(room)
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    this.toggleAnimation = function () {
+      this.animationsEnabled = !this.animationsEnabled;
+    };
+  }
+  angular
+    .module('blocChat')
+    .controller('ModalCtrl',['$uibModal','$log','Room', ModalCtrl])
+})();
